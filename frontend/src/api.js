e3e0a0b78fb166a8a7d1711bb52fce21ba40df73
unknown <sahanagowdasahana121@gmail.com>
2026-03-02 23:21:52 +0530

    api update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://agrilink-m2r1.onrender.com/api/health',
+    baseURL: 'https://agrilink-m2r1.onrender.com/api',
     headers: { 'Content-Type': 'application/json' }
 })
 
